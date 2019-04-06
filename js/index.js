53ec424e1974be1db42f8dc3521f8d6e2a0092e6
Omari Bennett <omari.bennett@gmail.com>
2019-04-06 15:56:46 -0400

    Initial project script

---
+++ js/index.js
@@ -0,0 +1,79 @@
+'use strict';
+// JavaScript Syntax
+//==========================================================================================================================================
+
+// /*
+//  Variable means anything that can vary.
+//  JavaScript includes variables which hold the data value and it can be changed anytime.
+//  JavaScript uses reserved keyword var to declare a variable. A variable must have a unique name.
+//  You can assign a value to a variable using equal to (=) operator when you declare it or before using it. 
+
+// Syntax:
+// 	var <variable-name>;
+
+// 	var <variable-name> = <value>;
+// */
+
+// var name = 'kydd';
+// console.log(name); // output: kydd
+
+// // you resign the variable later in the program
+// name = 'Osman';
+// console.log(name); // output: Osman
+
+// /*
+// const vs. let
+
+// The only difference between const and let is that const makes the contract that no rebinding will happen....
+// Given the above, const makes code easier to read: within its scope, a const variable always refers to the same object.
+// With let there is no such guarantee.
+// */
+
+// const theSkyIsBlue = 'blue';
+// // const variables cannot be updated
+// //theSkyIsBlue = 'red';		// TypeError: Assignment to constant variable.
+
+// let theCarIs = 'yellow';
+// // you can re-declaring the let variable
+// theCarIs = 'black';
+// console.log(theCarIs); // output: black
+
+//==========================================================================================================================================
+
+//==========================================================================================================================================
+//JavaScript Operators & Expressions
+// JavaScript uses arithmetic operators ( + - * / ) to compute values
+
+// let a = 2;
+// let b = 4;
+
+// console.log( { Output: a + b} ); // output: 6
+// console.log( { Output: a - b} ); // output: -2
+// console.log( { Output: a * b} ); // output: 8
+// console.log( { Output: a / b} ); // output: 0.5
+
+
+//==========================================================================================================================================
+
+//==========================================================================================================================================
+
+// // concatenation 
+// // computer programming, string concatenation is the operation of joining character strings end-to-end.
+
+// let partA = 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification.';
+// let partB = 'It is a programming language that is characterized as dynamic, weakly typed, prototype-based and multi-paradigm';
+
+// console.log( partA + partB );
+// console.log('Hi my name is ' + name + '!');
+
+// let newQuestion = 'Now, go to this website ' + name + ' and complete the task.';
+// let website = 'https://javascript.info/variables';
+// console.log({
+// 	question: newQuestion,
+// 	link: website,
+// 	task1: 'Working with variables',
+// 	task2: 'Giving the right name',
+// 	task3: 'Uppercase const?',
+// })
+
+//==========================================================================================================================================
